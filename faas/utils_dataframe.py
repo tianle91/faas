b4dac8fe5715e9d4686e3aaf47a83f035ef102c5
Tianle Chen <tianlechen@gmail.com>
2021-11-08 22:04:35 -0500

    JoinableByRowID

--- faas/utils_dataframe.py
+++ faas/utils_dataframe.py
@@ -1,7 +1,10 @@
 from typing import List
 
+import pyspark.sql.functions as F
 from pyspark.sql import DataFrame
-from pyspark.sql.types import NumericType, StringType, TimestampType
+from pyspark.sql.dataframe import DataFrame
+from pyspark.sql.types import (DoubleType, NumericType, StringType,
+                               TimestampType)
 
 
 def get_non_numeric_columns(df: DataFrame) -> List[str]:
@@ -27,3 +30,16 @@ def validate_timestamp_types(df: DataFrame, cols: List[str]):
         dtype = df.schema[c].dataType
         if not isinstance(dtype, TimestampType):
             raise TypeError(f'Column {c} is {dtype} but is expected to be timestamp.')
+
+
+ROW_ID_COL = '__ROW_ID__'
+
+
+class JoinableByRowID:
+    def __init__(self, df: DataFrame):
+        self.df = df.withColumn(ROW_ID_COL, F.monotonically_increasing_id())
+
+    def join_by_row_id(self, x: List[float], column: str) -> DataFrame:
+        mapping = {i: float(val) for i, val in enumerate(x)}
+        udf = F.udf(lambda i: mapping[i], DoubleType())
+        return self.df.withColumn(column, udf(F.col(ROW_ID_COL)))
