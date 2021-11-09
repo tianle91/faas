9ea74bb4310af4392472b8e7e46070e397e539dc
Tianle Chen <tianlechen@gmail.com>
2021-11-08 23:07:26 -0500

    test_NumericScaler

--- tests/test_scaler.py
+++ tests/test_scaler.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from pyspark.sql import DataFrame, SparkSession
 
-from faas.scaler import StandardScaler, get_mean_std
+from faas.scaler import NumericScaler, StandardScaler, get_mean_std
 
 
 def create_test_df(spark: SparkSession, n: int = 100) -> DataFrame:
@@ -20,7 +20,7 @@ def test_get_mean_std(spark):
     assert mean > 0 and stddev > 0, str((mean, stddev))
 
 
-def test_StandardScaler(spark):
+def test_StandardScaler(spark: SparkSession):
     df = create_test_df(spark)
     expected = df.toPandas()
 
@@ -34,4 +34,18 @@ def test_StandardScaler(spark):
     transformed_df = sc.transform(df).drop('q')
     transformed_df.collect()
     actual = sc.inverse_transform(transformed_df).select('p', 'q').toPandas()
+    pd.testing.assert_frame_equal(left=expected, right=actual)
+
+
+def test_NumericScaler(spark: SparkSession):
+    n = 100
+    df = spark.createDataFrame(pd.DataFrame({
+        'q': [float(i) % 100 for i in range(n)],
+        'qbase': [(float(i) % 100) + 1 for i in range(n)],
+    }))
+    expected = df.toPandas()
+    ns = NumericScaler(column='q', base_column='qbase')
+    transformed_df = ns.transform(df).drop('q')
+    transformed_df.collect()
+    actual = ns.inverse_transform(transformed_df).select('q', 'qbase').toPandas()
     pd.testing.assert_frame_equal(left=expected, right=actual)
