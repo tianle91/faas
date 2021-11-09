414eae2365762bb0b3e64e769447993d37a3a88d
Tianle Chen <tianlechen@gmail.com>
2021-11-09 13:25:22 -0500

    Update base.py

--- faas/base.py
+++ faas/base.py
@@ -9,45 +9,57 @@ class BaseTransformer:
     """A BaseTransformer adds a single column, i.e. feature_column"""
 
     @property
-    def feature_column(self) -> str:
+    def feature_columns(self) -> List[str]:
         raise NotImplementedError
 
     def fit(self, df: DataFrame) -> BaseTransformer:
         return self
 
     def transform(self, df: DataFrame) -> DataFrame:
-        raise NotImplementedError
+        return df
 
 
 class InvertibleTransformer(BaseTransformer):
 
     def inverse_transform(self, df: DataFrame) -> DataFrame:
-        raise NotImplementedError
+        return df
+
+
+class SelectTransformer(BaseTransformer):
+
+    def __init__(self, columns: List[str]) -> None:
+        self.columns = columns
+
+    def transform(self, df: DataFrame) -> DataFrame:
+        return df.select(*self.columns)
 
 
 class Pipeline(BaseTransformer):
-    def __init__(self, steps: List[Tuple[str, BaseTransformer]]):
+    def __init__(self, steps: List[BaseTransformer]):
         self.steps = steps
 
     @property
     def feature_columns(self) -> List[str]:
-        return [transformer.feature_column for _, transformer in self.steps]
+        res = []
+        for transformer in self.steps:
+            res.append(transformer.feature_columns)
+        return res
 
     def fit(self, df: DataFrame) -> Pipeline:
-        for _, transformer in self.steps:
+        for transformer in self.steps:
             transformer.fit(df)
         return self
 
     def transform(self, df: DataFrame) -> DataFrame:
-        for _, transformer in self.steps:
+        for transformer in self.steps:
             df = transformer.transform(df)
         return df
 
     def inverse_transform(self, df: DataFrame) -> DataFrame:
-        for name, transformer in self.steps[::-1]:
+        for transformer in self.steps[::-1]:
             if not isinstance(transformer, InvertibleTransformer):
                 raise TypeError(
-                    f'Transformer {name}, {transformer} should be InvertibleTransformer')
+                    f'Transformer {transformer} should be InvertibleTransformer')
             transformer: InvertibleTransformer = transformer
             df = transformer.inverse_transform(df)
         return df
