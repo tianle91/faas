e4f57150a5397c3c9883c7bc721db16790b10c15
Tianle Chen <tianlechen@gmail.com>
2021-11-09 21:31:45 -0500

    add logo

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 from ui import pages
 
-st.sidebar.title('FaaS')
+st.sidebar.title('FaaS 🌲')
 
 page_selector = st.sidebar.radio(label='page', options=pages.keys())
 if page_selector is not None:
