89940dedd700d974aa58651942c725952ce8eb78
Tianle Chen <tianlechen@gmail.com>
2021-11-10 09:01:32 -0500

    less verbose

--- ui/checklist.py
+++ ui/checklist.py
@@ -15,7 +15,6 @@ def run_target_checklist(e2e: E2EPipline, df: DataFrame) -> bool:
 
 def run_numeric_features_checklist(e2e: E2EPipline, df: DataFrame) -> bool:
     ok, messages = check_numeric(e2e, df=df)
-    st.markdown('Numeric features: ' + '✅' if ok else '❌')
     if not ok:
         for message in messages:
             st.error(message)
@@ -24,7 +23,6 @@ def run_numeric_features_checklist(e2e: E2EPipline, df: DataFrame) -> bool:
 
 def run_categorical_features_checklist(e2e: E2EPipline, df: DataFrame) -> bool:
     ok, messages = check_categorical(e2e, df=df)
-    st.markdown('Categorical features: ' + '✅' if ok else '❌')
     if not ok:
         for message in messages:
             st.error(message)
@@ -36,4 +34,7 @@ def run_features_checklist(e2e: E2EPipline, df: DataFrame) -> bool:
     categorical_ok = run_categorical_features_checklist(e2e, df=df)
     ok = numeric_ok and categorical_ok
     st.markdown('All features: ' + '✅' if ok else '❌')
+    if not ok:
+        st.markdown('Numeric features: ' + '✅' if numeric_ok else '❌')
+        st.markdown('Categorical features: ' + '✅' if categorical_ok else '❌')
     return ok
