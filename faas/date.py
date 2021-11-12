46a582c8683dc64ae9d6823659b20e4479f29624
Tianle Chen <tianlechen@gmail.com>
2021-11-12 14:53:53 -0500

    all the features

--- faas/date.py
+++ faas/date.py
@@ -1,29 +1,93 @@
-from typing import List
+import math
+from datetime import date
+from typing import Dict, List, Tuple
 
+import pyspark.sql.functions as F
 from pyspark.sql import DataFrame
+from pyspark.sql.types import FloatType
 
 from faas.base import BaseTransformer
+from faas.utils_dataframe import validate_date_types
 
 
-class SeasonalFeatures(BaseTransformer):
+def normalized_sine(x: float, period: float, phase: int):
+    x = (x + phase) / period
+    return math.sin(x)
 
-    def __init__(
-        self,
-        date_column: str,
-        day_of_week: bool = True,
-        day_of_month: bool = True,
-        day_of_year: bool = True,
-        week_of_year: bool = True,
-    ) -> None:
+
+class SeasonalityFeature(BaseTransformer):
+
+    def __init__(self, date_column: str) -> None:
         self.date_column = date_column
-        self.day_of_week = day_of_week
-        self.day_of_month = day_of_month
-        self.day_of_year = day_of_year
-        self.week_of_year = week_of_year
+
+    def get_value(self, dt: date) -> float:
+        raise NotImplementedError
 
     @property
+    def period(self) -> Tuple[str, float]:
+        raise NotImplementedError
+
+    @property
+    def feature_to_udf_mapping(self) -> Dict[str, callable]:
+        out = {}
+        period_name, period_value = self.period
+        i = 0
+        while i <= period_value:
+            out[f'Seasonality_{period_name}_{i}'] = F.udf(
+                lambda dt: normalized_sine(
+                    self.get_value(dt),
+                    period=self.period,
+                    phase=i
+                ),
+                FloatType()
+            )
+            i += 1
+        return out
+
+    @ property
     def feature_columns(self) -> List[str]:
-        return super().feature_columns
+        return list(self.feature_to_udf_mapping.keys())
 
     def transform(self, df: DataFrame) -> DataFrame:
-        pass
+        validate_date_types(df, cols=[self.date_column])
+        distincts = df.select(F.col(self.date_column))
+        for feature_column, udf in self.feature_to_udf_mapping.items():
+            distincts = distincts.withColumn(feature_column, udf(F.col(self.date_column)))
+        df = df.join(distincts, on=self.date_column, how='left')
+        return df
+
+
+def get_dow(dt: date) -> float:
+    return dt.weekday()
+
+
+class DayOfWeekFeatures(BaseTransformer):
+    get_value = get_dow
+    period = ('WeekOfDay', 6)
+
+
+def get_dom(dt: date) -> float:
+    return dt.day
+
+
+class DayOfMonthFeatures(BaseTransformer):
+    get_value = get_dom
+    period = ('DayOfMonth', 31)
+
+
+def get_doy(dt: date) -> float:
+    return (dt - date(dt.year, 1, 1)).days
+
+
+class DayOfYearFeatures(BaseTransformer):
+    get_value = get_doy
+    period = ('DayOfyear', 365.25)
+
+
+def get_woy(dt: date) -> float:
+    return dt.isocalendar()[1]
+
+
+class WeekOfYearFeatures(BaseTransformer):
+    get_value = get_woy
+    period = ('WeekOfYear', 52.1429)
