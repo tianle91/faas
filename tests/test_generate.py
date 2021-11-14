858c88c531a8e69eca6ba8bd48b2bc0f2f784b45
Tianle Chen <tianlechen@gmail.com>
2021-11-13 23:36:59 -0500

    Create test_generate.py

---
+++ tests/test_generate.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+from faas.generate import GenerateSynthetic
+
+
+def test_iid():
+    df = pd.DataFrame(GenerateSynthetic().generate_iid(n=100))
+    assert len(df) == 100
+
+
+def test_ts():
+    df = pd.DataFrame(GenerateSynthetic().generate_ts(num_days=100))
+    assert len(df) == 100
+
+
+def test_multi_ts():
+    df = pd.DataFrame(GenerateSynthetic().generate_multi_ts(ts_types=['a', 'b'], num_days=100))
+    assert len(df) == 200
+
+
+def test_multi_ts():
+    df = pd.DataFrame(GenerateSynthetic().generate_spatial(num_locations=100))
+    assert len(df) == 100
