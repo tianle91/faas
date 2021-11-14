8e501dbdceacc25f603858e1f418290a57dfc6de
Tianle Chen <tianlechen@gmail.com>
2021-11-13 20:49:06 -0500

    move generate to faas

--- faas/generate.py
+++ faas/generate.py
@@ -1,5 +1,5 @@
 from datetime import date, timedelta
-from typing import Dict, List
+from typing import Dict, List, Tuple
 
 from numpy.random import default_rng
 
@@ -17,17 +17,17 @@ class GenerateSynthetic:
 
     @property
     def categorical_names(self) -> List[str]:
-        return [f'categorical_{i}' for i in self.num_categorical]
+        return [f'categorical_{i}' for i in range(self.num_categorical)]
 
     @property
     def numeric_names(self) -> List[str]:
-        return [f'numeric_{i}' for i in self.num_numeric]
+        return [f'numeric_{i}' for i in range(self.num_numeric)]
 
     def generate_iid(self, n: int = 1000) -> Dict[str, list]:
         res = {}
         for c in self.categorical_names:
             res[c] = self.rng.choice(ALPHABET, size=n)
-        for c in self.num_numeric:
+        for c in self.numeric_names:
             res[c] = self.rng.standard_normal(size=n)
         return res
 
@@ -36,11 +36,21 @@ class GenerateSynthetic:
         num_days: int = 750,
         date_column: str = 'date'
     ) -> Dict[str, list]:
-        res = {
-            date_column: [date(2000, 1, 1) + timedelta(days=i) for i in range(num_days)]
-        }
-        for c in self.categorical_names:
-            res[c] = self.rng.choice(ALPHABET, size=num_days)
-        for c in self.num_numeric:
-            res[c] = self.rng.standard_normal(size=num_days)
+        res = self.generate_iid(n=num_days)
+        res[date_column] = [date(2000, 1, 1) + timedelta(days=i) for i in range(num_days)]
+        return res
+
+    def generate_multi_ts(
+        self,
+        ts_types: List[str],
+        ts_type_column: str = 'ts_type',
+        num_days: int = 750,
+        date_column: str = 'date'
+    ) -> Dict[str, list]:
+        num_ts = len(ts_types)
+        res = self.generate_iid(n=num_ts * num_days)
+        res[date_column] = [date(2000, 1, 1) + timedelta(days=i) for i in range(num_days)] * num_ts
+        res[ts_type_column] = []
+        for ts_type in ts_types:
+            res[ts_type_column] += [ts_type, ] * num_days
         return res
