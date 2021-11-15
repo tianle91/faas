e0e3bd05742214c9a6ba39aa30444c8dacf1de2b
Tianle Chen <tianlechen@gmail.com>
2021-11-15 00:20:56 -0500

    numeric_with_few_distincts

--- faas/utils/types.py
+++ faas/utils/types.py
@@ -1,4 +1,8 @@
+from typing import List
+
 from pyspark.sql import DataFrame, SparkSession
+
+from faas.utils.dataframe import get_numeric_columns
 
 DEFAULT_DATE_FORMAT = 'yyyy-MM-dd'
 
@@ -12,3 +16,11 @@ def load_csv(spark: SparkSession, p: str) -> DataFrame:
         .load(p)
     )
     return df
+
+
+def numeric_with_few_distincts(df: DataFrame, num_distincts: int = 10) -> List[str]:
+    out = []
+    for c in get_numeric_columns(df):
+        if df.select(c).distinct().count() < num_distincts:
+            out.append(c)
+    return out
