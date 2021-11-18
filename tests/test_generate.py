b42efa6e30eaa890187d441ee7f00fbb7a52dd65
Tianle Chen <tianlechen@gmail.com>
2021-11-17 21:59:46 -0500

    Update test_generate.py

--- tests/test_generate.py
+++ tests/test_generate.py
@@ -1,11 +1,15 @@
 import pandas as pd
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType
 
-from faas.generate import GenerateSynthetic
+from faas.generate import GenerateSynthetic, convert_dict_to_list
 
 
-def test_iid():
-    df = pd.DataFrame(GenerateSynthetic().generate_iid(n=100))
-    assert len(df) == 100
+def test_iid(spark: SparkSession):
+    d = GenerateSynthetic().generate_iid(n=100)
+    assert len(pd.DataFrame(d)) == 100
+    sdf = spark.createDataFrame(data=convert_dict_to_list(d))
+    assert sdf.count() == 100
 
 
 def test_ts():
@@ -21,3 +25,9 @@ def test_multi_ts():
 def test_spatial():
     df = pd.DataFrame(GenerateSynthetic().generate_spatial(num_locations=100))
     assert len(df) == 100
+
+
+def test_convert_dict_to_list():
+    d = GenerateSynthetic().generate_iid(n=100)
+    actual = convert_dict_to_list(d=d)
+    assert len(actual) == 100
