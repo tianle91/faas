5c76a01ffabe712909c9fcdc70bd10428c697b72
Tianle Chen <tianlechen@gmail.com>
2021-11-19 16:51:53 -0500

    update YTransformer

--- faas/etl.py
+++ faas/etl.py
@@ -10,7 +10,7 @@ from faas.transformer.base import BaseTransformer, Passthrough, Pipeline
 from faas.transformer.date import (DayOfMonthFeatures, DayOfWeekFeatures,
                                    DayOfYearFeatures, WeekOfYearFeatures)
 from faas.transformer.encoder import OrdinalEncoder
-from faas.transformer.scaler import StandardScaler
+from faas.transformer.scaler import StandardScaler, LogTransform, NumericScaler
 from faas.transformer.weight import Normalize
 
 logger = logging.getLogger(__name__)
@@ -26,7 +26,7 @@ class PipelineTransformer(BaseTransformer):
     def feature_columns(self) -> List[str]:
         return self.pipeline.feature_columns
 
-    def fit(self, df: DataFrame) -> XTransformer:
+    def fit(self, df: DataFrame) -> PipelineTransformer:
         self.pipeline.fit(df)
         return self
 
@@ -64,19 +64,48 @@ class XTransformer(PipelineTransformer):
 
 
 class YTransformer(PipelineTransformer):
-    def __init__(self, target_column: str, group_column: Optional[str] = None):
+    def __init__(
+        self,
+        target_column: str,
+        log_transform: bool,
+        normalize_by_categorical: Optional[str] = None,
+        normalize_by_numerical: Optional[str] = None,
+    ):
         self.target_column = target_column
-        self.group_column = group_column
+        self.log_transform = log_transform
+        if normalize_by_categorical is not None and normalize_by_numerical is not None:
+            raise ValueError('Cannot normalize by both categorical and numerical.')
+        self.normalize_by_categorical = normalize_by_categorical
+        self.normalize_by_numerical = normalize_by_numerical
         # create pipeline
         steps = []
-        if group_column is not None:
-            steps.append(StandardScaler(column=target_column, group_column=group_column))
+        target_column = self.target_column
+        if self.log_transform:
+            step = LogTransform(column=target_column)
+            steps.append(step)
+            target_column = step.feature_column
+
+        if normalize_by_categorical is not None:
+            steps.append(StandardScaler(
+                column=target_column,
+                group_column=normalize_by_categorical
+            ))
+        elif normalize_by_numerical is not None:
+            steps.append(NumericScaler(
+                column=target_column,
+                group_column=normalize_by_numerical
+            ))
         else:
             steps.append(Passthrough(columns=[target_column]))
         self.pipeline = Pipeline(steps)
 
+    @property
+    def feature_columns(self) -> List[str]:
+        return [self.pipeline.feature_columns[-1]]
+
 
 class WTransformer(PipelineTransformer):
-    def __init__(self, date_column: str):
-        self.date_column = date_column
-        self.pipeline = Pipeline([Normalize(group_column=date_column)])
+    def __init__(self, group_columns: str):
+        self.group_columns = group_columns
+        steps = [Normalize(group_column=c) for c in group_columns]
+        self.pipeline = Pipeline(steps)
