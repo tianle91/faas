641ad6fb2f9f5d0abf7c97b6a4a3a59e11fb38d3
Tianle Chen <tianlechen@gmail.com>
2021-11-19 23:52:27 -0500

    test_recommend

--- faas/tests/test_wrapper.py
+++ faas/tests/test_wrapper.py
@@ -1,16 +1,29 @@
 from pyspark.sql import SparkSession
 
 from faas.generate import GenerateSynthetic, convert_dict_to_list
-from faas.wrapper import ETLWrapperForLGBM
+from faas.wrapper import ETLConfig, ETLWrapperForLGBM, recommend
 
 
 def test_ETLWrapperForLGBM(spark: SparkSession):
-    ewlgbm = ETLWrapperForLGBM(
+    conf = ETLConfig(
         target_column='numeric_0',
         x_categorical_columns=['categorical_0'],
         x_numeric_features=['numeric_1'],
     )
+    ewlgbm = ETLWrapperForLGBM(conf)
     d = GenerateSynthetic(num_categorical=2, num_numeric=2)
     df = spark.createDataFrame(data=convert_dict_to_list(d.generate_iid()))
     ewlgbm.fit(df)
     ewlgbm.predict(df)
+
+
+def test_recommend(spark: SparkSession):
+    d = GenerateSynthetic(num_categorical=2, num_numeric=2)
+    df = spark.createDataFrame(data=convert_dict_to_list(d.generate_iid()))
+    actual, messages = recommend(df=df, target_column='numeric_0')
+    expected = ETLConfig(
+        x_categorical_columns=['categorical_0', 'categorical_1'],
+        x_numeric_features=['numeric_1'],
+        target_column='numeric_0',
+    )
+    assert actual == expected, ' '.join(messages)
--- faas/wrapper.py
+++ faas/wrapper.py
@@ -90,26 +90,15 @@ class ETLWrapperForLGBM:
         return df_pred
 
 
-def all_non_negative(df: DataFrame, c: str) -> bool:
-    IS_NON_NEG = '__IS_NON_NEG__'
-    n = df.count()
-    df = (
-        df
-        .withColumn(IS_NON_NEG, F.col(c))
-        .groupBy(IS_NON_NEG).count()
-    )
-    d = {
-        row[IS_NON_NEG]: row['count']
-        for row in df.collect()
-    }
-    return d[True] == n
+def min_val(df: DataFrame, c: str) -> bool:
+    return df.agg(F.min(c).alias('min')).collect()[0]['min']
 
 
 def get_top_correlated(df: DataFrame, c: str) -> Tuple[Optional[str], Optional[float]]:
     ABS_VAL_COL = '__ABS_VAL_COL__'
     corr_df = correlation(
         df=df,
-        feature_columns=[c for c in df.colums if c != c],
+        feature_columns=[c for c in df.columns if c != c],
         target_column=c
     )
     corr_df.drop(c)
@@ -122,14 +111,47 @@ def get_top_correlated(df: DataFrame, c: str) -> Tuple[Optional[str], Optional[f
 
 
 def recommend(df: DataFrame, target_column: str) -> ETLConfig:
-    conf = ETLConfig(target_column=target_column)
     msgs = []
+
+    feature_cols = [c for c in df.columns if c != target_column]
+    date_column = None
+    x_categorical_columns = []
+    x_numeric_features = []
+    weight_group_columns = []
+    for c in feature_cols:
+        dtype = df.schema[c].dataType
+        print(c, dtype)
+        if isinstance(dtype, DateType):
+            date_column = c
+            weight_group_columns.append(c)
+        elif isinstance(dtype, NumericType):
+            x_numeric_features.append(c)
+        elif isinstance(dtype, StringType):
+            x_categorical_columns.append(c)
+        else:
+            msgs.append(f'Ignoring column {c}')
+
+    conf = ETLConfig(
+        target_column=target_column,
+        x_categorical_columns=x_categorical_columns,
+        x_numeric_features=x_numeric_features,
+    )
+    msgs.append(f'Setting x_categorical_columns as {x_categorical_columns}')
+    msgs.append(f'Setting x_numeric_features as {x_numeric_features}')
+    if date_column is not None:
+        conf.date_column = date_column
+        msgs.append(f'Setting date_column to {c} as it is a DateType')
+    if len(weight_group_columns) > 0:
+        conf.weight_group_columns = weight_group_columns
+        msgs.append(f'Setting weight_group_columns as {weight_group_columns}')
+
     # target_log_transform
-    if all_non_negative:
+    min_target_val = min_val(df=df, c=target_column)
+    if min_target_val >= 0.:
         conf.target_log_transform = True
         msgs.append(
             'Setting target_log_transform to True '
-            'since all target values are non negative.'
+            f'since min_target_val = {min_target_val}'
         )
     # TODO target_normalize_by_categorical
     # target_normalize_by_numerical
@@ -140,30 +162,4 @@ def recommend(df: DataFrame, target_column: str) -> ETLConfig:
             f'Setting target_normalize_by_numerical to {top_corr_col} '
             f'since correlation with target {target_column} is {top_corr_val} (> 0.5)'
         )
-
-    feature_cols = [c for c in df.columns if c != target_column]
-    x_categorical_columns = []
-    x_numeric_features = []
-    weight_group_columns = []
-    for c in feature_cols:
-        dtype = df.schema[c].dataType
-        if isinstance(dtype, DateType):
-            conf.date_column = c
-            msgs.append(f'Setting date_column to {c} as it is a DateType')
-            weight_group_columns.append(c)
-        elif isinstance(dtype, NumericType):
-            x_numeric_features.append(c)
-        elif isinstance(dtype, StringType):
-            x_categorical_columns.append(c)
-        else:
-            msgs.append(f'Ignoring column {c}')
-        feature_cols.pop(feature_cols.index(c))
-
-    conf.x_categorical_columns = x_categorical_columns
-    msgs.append(f'Setting x_categorical_columns as {x_categorical_columns}')
-    conf.x_numeric_features = x_numeric_features
-    msgs.append(f'Setting x_numeric_features as {x_numeric_features}')
-    if len(weight_group_columns) > 0:
-        conf.weight_group_columns = weight_group_columns
-        msgs.append(f'Setting weight_group_columns as {weight_group_columns}')
-    return conf
+    return conf, msgs
