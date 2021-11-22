150e2d8820c8d8a752cf9494b26f3f93b86679d2
Tianle Chen <tianlechen@gmail.com>
2021-11-21 22:27:24 -0500

    reduce repetition

--- faas/transformer/utils.py
+++ faas/transformer/utils.py
@@ -1,25 +1,23 @@
 from typing import List
 
 from pyspark.sql import DataFrame
-from pyspark.sql.types import DateType, NumericType, StringType
+from pyspark.sql.types import DataType, DateType, NumericType, StringType
+
+
+def validate_types(df: DataFrame, cols: List[str], expected_dtype: DataType):
+    for c in cols:
+        dtype = df.schema[c].dataType
+        if not isinstance(dtype, expected_dtype):
+            raise TypeError(f'Column {c} is {dtype} but is expected to be {expected_dtype}.')
 
 
 def validate_numeric_types(df: DataFrame, cols: List[str]):
-    for c in cols:
-        dtype = df.schema[c].dataType
-        if not isinstance(dtype, NumericType):
-            raise TypeError(f'Column {c} is {dtype} but is expected to be numeric.')
+    return validate_types(df=df, cols=cols, expected_dtype=NumericType)
 
 
 def validate_categorical_types(df: DataFrame, cols: List[str]):
-    for c in cols:
-        dtype = df.schema[c].dataType
-        if not isinstance(dtype, StringType):
-            raise TypeError(f'Column {c} is {dtype} but is expected to be string.')
+    return validate_types(df=df, cols=cols, expected_dtype=StringType)
 
 
 def validate_date_types(df: DataFrame, cols: List[str]):
-    for c in cols:
-        dtype = df.schema[c].dataType
-        if not isinstance(dtype, DateType):
-            raise TypeError(f'Column {c} is {dtype} but is expected to be timestamp.')
+    return validate_types(df=df, cols=cols, expected_dtype=DateType)
