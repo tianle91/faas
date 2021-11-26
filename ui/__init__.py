9199ba18ca647f05486db42470367d3f1072ee42
Tianle Chen <tianlechen@gmail.com>
2021-11-26 12:36:10 -0500

    list

--- ui/__init__.py
+++ ui/__init__.py
@@ -1,7 +1,9 @@
 from .predict import run_predict
 from .training import run_training
+from .list import run_list
 
 pages = {
     'Training': run_training,
     'Predict': run_predict,
+    'List': run_list
 }
