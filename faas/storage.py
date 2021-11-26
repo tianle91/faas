5ecba5c053d86597fbedb322d0840ac06b0ffd31
Tianle Chen <tianlechen@gmail.com>
2021-11-26 12:10:19 -0500

    Update storage.py

--- faas/storage.py
+++ faas/storage.py
@@ -1,10 +1,13 @@
 import logging
+import pprint as pp
 import random
 import string
 from datetime import datetime
+from typing import Tuple
 
 from sqlitedict import SqliteDict
 
+from faas.config import Config
 from faas.lightgbm import ETLWrapperForLGBM
 
 logger = logging.getLogger(__name__)
@@ -26,29 +29,32 @@ def create_key() -> str:
         return k
 
 
-def list_models() -> dict:
-    d_print = {}
-    with SqliteDict(MODEL_STORE) as d:
-        for k, v in d.items():
-            dt, e = v
-            e: ETLWrapperForLGBM = e
-            d_print[k] = {'timestamp': dt, 'config': e.config}
-    return d_print
-
-
-def write_model(model: ETLWrapperForLGBM) -> str:
+def write_model(model: ETLWrapperForLGBM, conf: Config) -> str:
     with SqliteDict(MODEL_STORE) as d:
         key = create_key()
-        d[key] = (datetime.now(), model)
+        dt = datetime.now()
+        d[key] = (dt, model, conf)
         d.commit()
+        logger.info(f'Wrote model with key: {key} at {dt}')
         logger.info(f'Model store at: {MODEL_STORE} is now {len(d)}')
     return key
 
 
-def read_model(key: str) -> ETLWrapperForLGBM:
+def read_model(key: str) -> Tuple[ETLWrapperForLGBM, Config]:
     with SqliteDict(MODEL_STORE) as d:
         if key not in d:
             raise KeyError(f'Key: {key} not found!')
         else:
-            _, model = d[key]
-            return model
+            dt, model, conf = d[key]
+            logger.info(f'Read model with key: {key} created at {dt}')
+            return model, conf
+
+
+def list_models() -> dict:
+    l = []
+    with SqliteDict(MODEL_STORE) as d:
+        for key in d:
+            dt, _, conf = d[key]
+            l.append(f'dt: {dt} key: {key}\n{pp.pformat(conf.__dict)}')
+    sep = '\n' + '-' * 1000 + '\n'
+    return sep.join(l)
