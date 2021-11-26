343c0dc8f22c3f729eca2c9e353f4509a77133ad
Tianle Chen <tianlechen@gmail.com>
2021-11-26 12:49:03 -0500

    Update list.py

--- ui/list.py
+++ ui/list.py
@@ -1,9 +1,18 @@
+import pprint as pp
+
 import streamlit as st
 
 from faas.storage import list_models
-import pprint as pp
+from ui.vis_lightgbm import get_vis_lgbmwrapper
 
 
 def run_list():
     all_models = list_models()
-    st.code(pp.pformat(all_models))
+    for k in all_models:
+        model_key, dt = k
+        with st.expander(f'Model: {model_key} created at: {dt}'):
+            st.code(model_key)
+            m, conf = all_models[k]
+            st.code(pp.pformat(conf.__dict__, compact=True))
+            st.code(pp.pformat(m.config.to_dict(), compact=True))
+            get_vis_lgbmwrapper(m)
