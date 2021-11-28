b8cdc7f33e81decc69f3deb6e2f3a99680a86f59
Tianle Chen <tianlechen@gmail.com>
2021-11-28 09:44:04 -0500

    fix non-deterministic test

--- faas/eda/tests/test_iid.py
+++ faas/eda/tests/test_iid.py
@@ -6,14 +6,22 @@ from faas.eda.iid import correlation
 
 def test_correlation(spark: SparkSession):
     n = 100
-    c_l = [c for c in 'ab']
+    c_l = ['a', 'b', 'c']
+    v_l = [0, 0, 1]
     pdf = pd.DataFrame({
         'cat': [c_l[i % len(c_l)] for i in range(n)],
-        'num': [i % len(c_l) for i in range(n)],
+        'num': [v_l[i % len(v_l)] for i in range(n)],
     })
     df = spark.createDataFrame(pdf)
 
     cols = ['cat', 'num']
-    actual = correlation(df=df, columns=cols)
-    expected = pd.DataFrame([[1., 1., ], [1., 1., ]], index=cols, columns=cols)
+    actual = correlation(df=df, columns=cols, collapse_categorical=True)
+    expected = pd.DataFrame(
+        [
+            [1., 1., ],
+            [1., 1., ]
+        ],
+        index=cols,
+        columns=cols
+    )
     pd.testing.assert_frame_equal(actual, expected, rtol=1e-3)
