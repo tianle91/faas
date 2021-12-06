e275f954d49ac982b378afe5b0120d2f8be82b37
Tianle Chen <tianlechen@gmail.com>
2021-12-05 21:09:17 -0500

    evaluate as page

--- ui/__init__.py
+++ ui/__init__.py
@@ -1,3 +1,4 @@
+from .evaluate import run_evaluation
 from .list import run_list
 from .predict import run_predict
 from .training import run_training
@@ -5,5 +6,6 @@ from .training import run_training
 pages = {
     'Training': run_training,
     'Predict': run_predict,
+    'Evaluate': run_evaluation,
     'List': run_list
 }
