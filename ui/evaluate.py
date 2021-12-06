42ffbf3bd1a4d613dba3ed80b751d83de8ae2bdd
Tianle Chen <tianlechen@gmail.com>
2021-12-05 21:09:23 -0500

    Update evaluate.py

--- ui/evaluate.py
+++ ui/evaluate.py
@@ -1,46 +1,30 @@
-import pprint as pp
-
 import streamlit as st
 from pyspark.sql import DataFrame
 
 from faas.storage import read_model
 from faas.utils.dataframe import has_duplicates
-from ui.visualization.vis_iid import vis_evaluate_iid
-from ui.visualization.vis_lightgbm import get_vis_lgbmwrapper
+from ui.evaluation.vis_iid import vis_evaluate_iid
 
 
 def run_evaluation():
     st.title('Evaluation')
 
-    model_key = st.session_state.get('model_key', '')
+    df_predict: DataFrame = st.session_state.get('df_predict', None)
+    df_actual: DataFrame = st.session_state.get('df_actual', None)
 
-    stored_model = None
-    if model_key != '':
-        try:
-            stored_model = read_model(key=model_key)
-            st.session_state['model_key'] = model_key
-        except KeyError as e:
-            st.error(e)
-
-    if stored_model is not None:
-        st.success('Model loaded!')
-        with st.expander('Model visualization'):
-            st.code(pp.pformat(stored_model.config.__dict__))
-            get_vis_lgbmwrapper(stored_model.m)
-
+    if df_predict is None or df_actual is None:
+        st.error('Upload dataframe with actuals and run predictions for evaluation.')
+    else:
+        model_key = st.session_state.get('model_key')
+        stored_model = read_model(key=model_key)
         config = stored_model.config
-
-        df_predict: DataFrame = st.session_state.get('df_predict', None)
-        df_actual: DataFrame = st.session_state.get('df_actual', None)
 
         config.validate_df(df=df_predict)
         config.validate_df(df=df_actual)
 
-        df_predict = df_predict.select(*config.used_columns_prediction)
-        if has_duplicates(df_predict):
+        if has_duplicates(df_predict.select(*config.used_columns_prediction)):
             raise ValueError('Cannot evaluate as df_predict has duplicate feature columns.')
-        df_actual = df_actual.select(*config.used_columns_prediction)
-        if has_duplicates(df_actual):
+        if has_duplicates(df_actual.select(*config.used_columns_prediction)):
             raise ValueError('Cannot evaluate as df_actual has duplicate feature columns.')
 
         vis_evaluate_iid(df_predict=df_predict, df_actual=df_actual, config=config)
