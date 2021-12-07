81f4759530a9ced785989bdfea38a92b744deab3
Tianle Chen <tianlechen@gmail.com>
2021-12-06 22:06:28 -0500

    purely categorical or numeric

--- generate_sample_data.py
+++ generate_sample_data.py
@@ -24,3 +24,17 @@ df.toPandas().to_csv('data/sample_multi_ts.csv', index=False)
 dict_of_lists = gs.generate_spatial()
 df: DataFrame = spark.createDataFrame(data=convert_dict_to_list(dict_of_lists))
 df.toPandas().to_csv('data/sample_spatial.csv', index=False)
+
+# sample numeric iid
+gs_numeric = GenerateSynthetic(num_categorical=0, num_numeric=10)
+
+dict_of_lists = gs_numeric.generate_iid()
+df: DataFrame = spark.createDataFrame(data=convert_dict_to_list(dict_of_lists))
+df.toPandas().to_csv('data/sample_numeric_iid.csv', index=False)
+
+# sample categorical iid
+gs_categorical = GenerateSynthetic(num_categorical=10, num_numeric=0)
+
+dict_of_lists = gs_categorical.generate_iid()
+df: DataFrame = spark.createDataFrame(data=convert_dict_to_list(dict_of_lists))
+df.toPandas().to_csv('data/sample_categorical_iid.csv', index=False)
