5fa0706efb78d166329c6adb20227d38c55a5444
Tianle Chen <tianlechen@gmail.com>
2021-12-08 23:57:06 -0500

    prompt to training

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import streamlit as st
 from ui import pages
 
 st.sidebar.title('FaaS 🌲')
+st.sidebar.markdown('To start, head to Training.')
 
 page_selector = st.sidebar.radio(label='page', options=pages.keys())
 if page_selector is not None:
