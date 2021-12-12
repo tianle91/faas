367aa17ad5eb6ea4d5d8572c3ed8e0707fbdca92
Tianle Chen <tianlechen@gmail.com>
2021-12-11 19:14:46 -0500

    better listing

--- ui/list.py
+++ ui/list.py
@@ -3,21 +3,27 @@ import pprint as pp
 import streamlit as st
 
 from faas.storage import list_models
-from ui.visualization.vis_lightgbm import get_vis_lgbmwrapper
 
 
 def run_list(st_container=None):
     if st_container is None:
         st_container = st
 
-    all_models = list_models()
+    all_models = [(model_key, stored_model) for model_key, stored_model in list_models().items()]
+    all_models.sort(key=lambda v: v[1].dt, reverse=True)
+
     if len(all_models) == 0:
-        st.warning('No trained models yet!')
-    for model_key, stored_model in all_models.items():
-        with st.expander(f'Model: {model_key} created at: {stored_model.dt}'):
-            st.markdown(
-                f'model_key: `{model_key}` '
-                f'num_calls_remaining: {stored_model.num_calls_remaining}'
-            )
-            st.code(pp.pformat(stored_model.config.__dict__, compact=True))
-            get_vis_lgbmwrapper(stored_model.m, st_container=st_container)
+        st_container.warning('No trained models yet!')
+
+    for model_key, stored_model in all_models:
+        st_container.markdown(
+            f'''
+            ----
+            Model key: `{model_key}`
+
+            Created at: `{stored_model.dt}`
+
+            Number of predictions remaining: `{stored_model.num_calls_remaining}`
+            '''
+        )
+        st_container.code(pp.pformat(stored_model.config.__dict__, compact=True))
