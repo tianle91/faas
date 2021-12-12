c1678b1dfcf5249797a67e9a312d326ae3a3f2d6
Tianle Chen <tianlechen@gmail.com>
2021-12-11 19:14:51 -0500

    highlight_columns

--- ui/visualization/vis_df.py
+++ ui/visualization/vis_df.py
@@ -1,3 +1,6 @@
+from typing import List
+
+import pandas as pd
 import streamlit as st
 from pyspark.sql import DataFrame
 
@@ -9,3 +12,14 @@ def preview_df(df: DataFrame, n: int = 100, st_container=None):
     preview_pdf = df.limit(n).toPandas()
     st_container.markdown(f'Preview for first {n} rows out of {df.count()} loaded.')
     st_container.dataframe(preview_pdf)
+
+
+def highlight_columns(df: pd.DataFrame, columns: List[str]):
+
+    def fn(s: pd.Series) -> List[str]:
+        if s.name in columns:
+            return ['background-color: #ff0000; color: white'] * len(s)
+        else:
+            return [''] * len(s)
+
+    return df.style.apply(fn, axis=0)
