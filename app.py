1b7cb7b7f64e62add1d5a8ef571ffa6ae2cfa4f0
Tianle Chen <tianlechen@gmail.com>
2021-12-12 23:00:48 -0500

    put loading and summary behind cache

--- app.py
+++ app.py
@@ -5,24 +5,34 @@ from faas.storage import read_model
 from faas.utils.io import load_cached_df_from_st_uploaded
 from ui import pages
 
-spark = (
-    SparkSession
-    .builder
-    .appName('ui')
-    .config('spark.driver.maxResultsSize', '16g')
-    .getOrCreate()
-)
+
+def get_spark():
+    spark = (
+        SparkSession
+        .builder
+        .appName('ui')
+        .config('spark.driver.maxResultsSize', '16g')
+        .getOrCreate()
+    )
+    return spark
+
+
+@st.cache(max_entries=1)
+def update_state_with_new_upload(f):
+    spark = get_spark()
+    df = load_cached_df_from_st_uploaded(f=f, spark=spark)
+    st.session_state['df'] = df
+    with st.spinner('Profiling dataframe...'):
+        summary_pdf = df.describe().toPandas().set_index(keys='summary')
+    st.session_state['summary_pdf'] = summary_pdf
+
 
 st.sidebar.title('FaaS 🌲')
 
 # populate states: df, summary_df
 f = st.sidebar.file_uploader('Upload dataframe', type=['csv', 'parquet'])
 if f is not None:
-    df = load_cached_df_from_st_uploaded(f=f, spark=spark)
-    st.session_state['df'] = df
-    with st.spinner('Profiling dataframe...'):
-        summary_pdf = df.describe().toPandas().set_index(keys='summary')
-    st.session_state['summary_pdf'] = summary_pdf
+    update_state_with_new_upload(f)
 
 # populate states: model_key, stored_model
 model_key = st.session_state.get('model_key', '')
@@ -35,7 +45,7 @@ if model_key_input != '':
     except Exception as e:
         st.sidebar.error(e)
 
-
+# set up the pages
 page_selector = st.sidebar.radio(label='Pages', options=pages.keys())
 if page_selector is not None:
     pages[page_selector](st.container())
