eb526e433676d7729a4807ccc7206bfb166e8e74
Tianle Chen <tianlechen@gmail.com>
2021-12-14 11:37:52 -0500

    test_LGBMWrapper_categorical_iid

--- faas/transformer/tests/test_lightgbm.py
+++ faas/transformer/tests/test_lightgbm.py
@@ -1,5 +1,6 @@
 import pytest
 from pyspark.sql import SparkSession
+from pyspark.sql.types import StringType
 
 from faas.generate import GenerateSynthetic, convert_dict_to_list
 from faas.transformer.etl import ETLConfig, FeatureConfig, TargetConfig
@@ -9,11 +10,11 @@ from faas.transformer.lightgbm import ETLWrapperForLGBM
 @pytest.mark.parametrize(
     ('num_categorical'),
     [
-        pytest.param(2, id='some categorical'),
-        pytest.param(0, id='no categorical'),
+        pytest.param(2, id='some categorical features'),
+        pytest.param(0, id='no categorical features'),
     ]
 )
-def test_LGBMWrapper_iid(spark: SparkSession, num_categorical: int):
+def test_LGBMWrapper_numeric_iid(spark: SparkSession, num_categorical: int):
     d = GenerateSynthetic(num_categorical=num_categorical, num_numeric=2)
     dict_of_lists = d.generate_iid()
     conf = ETLConfig(
@@ -27,6 +28,26 @@ def test_LGBMWrapper_iid(spark: SparkSession, num_categorical: int):
     df = spark.createDataFrame(data=convert_dict_to_list(dict_of_lists))
     ewlgbm.fit(df)
     ewlgbm.predict(df)
+
+
+def test_LGBMWrapper_categorical_iid(spark: SparkSession, num_categorical: int = 2):
+    d = GenerateSynthetic(num_categorical=num_categorical, num_numeric=2)
+    dict_of_lists = d.generate_iid()
+    conf = ETLConfig(
+        target=TargetConfig(
+            column=d.categorical_names[0],
+            is_categorical=True
+        ),
+        feature=FeatureConfig(
+            categorical_columns=d.categorical_names,
+            numeric_columns=d.numeric_names[1:],
+        )
+    )
+    ewlgbm = ETLWrapperForLGBM(conf)
+    df = spark.createDataFrame(data=convert_dict_to_list(dict_of_lists))
+    ewlgbm.fit(df)
+    df_predict = ewlgbm.predict(df)
+    assert isinstance(df_predict.schema[d.categorical_names[0]].dataType, StringType)
 
 
 def test_LGBMWrapper_ts(spark: SparkSession):
